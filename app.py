f7a13df0ab084027b0515795e6101d463683ab7c
noahricke <130416218+noahricke@users.noreply.github.com>
2026-04-26 16:38:33 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('My First Streamlit App')
+st.write('Hello! This app is live on Streamlit Community Cloud')
+Name = st.text_input('Enter your name:')
+if name:
+    st.write(f'Welcome, {name}!')
